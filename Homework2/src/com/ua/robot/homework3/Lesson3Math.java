287e8e89eab809f5c2a5a55b7ef3de736f848012
TonyMontana277 <gajjdukanton277@gmail.com>
2023-01-18 17:29:42 +0300

    Lesson 3 home-work

---
+++ Homework2/src/com/ua/robot/homework3/Lesson3Math.java
@@ -0,0 +1,20 @@
+package com.ua.robot.homework3;
+
+public class Lesson3Math {
+
+	public static void main(String[] args) {
+		// generate numbers
+		double a = Math.random();
+		double b = Math.random();
+		
+		a++;
+		b++;
+		double sum = a + b;// count sum 
+		double minus = a - b;// count subtraction
+		double multiply = a * b;// count multiplication
+		double divide = a / b;// count division
+		System.out.println("a = " + a + "\nb = " + b);
+		System.out.println("sum = " + sum + "\nsubtraction = " + minus + "\nmultiplication = " + multiply + "\ndivision = " + divide);
+	}
+
+}
