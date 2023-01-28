74c9e8e4f25a10e3412ec302ef9098f6095a9795
Anton Haiduk <gajjdukanton277@gmail.com>
2023-01-28 22:41:11 +0300

    lesson 6 home-work

---
+++ Homework2/src/com/ua/robot/homework6/EvenFromOneToHundred.java
@@ -0,0 +1,15 @@
+package com.ua.robot.homework6;
+
+public class EvenFromOneToHundred {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while (i < 101) {
+			i++;
+			if (i % 2 ==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
